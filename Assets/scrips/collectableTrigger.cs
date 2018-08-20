80b0f2079c99ea49d116fff05df50dc6d4213bab
Shmabulock <dima.kahnovich@gmail.com>
2018-08-20 15:58:06 +0300

    collectables
    created collectables that move walls back, need to make  collectables controller to  spawn them

---
+++ Assets/scrips/collectableTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class collectableTrigger : MonoBehaviour
+{
+    public string tagName;
+    public GameObject leftWall;
+    public GameObject rightWall;
+    public GameObject upWall;
+    public GameObject downWall;
+    public float bonusPower;
+    Vector3 vec;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == tagName)
+        {
+            vec.x = bonusPower/50;
+            
+
+             leftWall.transform.position = leftWall.transform.position - vec;
+             rightWall.transform.position = rightWall.transform.position + vec;
+             vec.x = 0;
+             vec.y = bonusPower/50;
+             upWall.transform.position = upWall.transform.position - vec;
+             downWall.transform.position = downWall.transform.position + vec;
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/scrips/loadSceneTrigger.cs
+++ Assets/scrips/loadSceneTrigger.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class stopTrigger : MonoBehaviour {
-
+public class loadSceneTrigger : MonoBehaviour {
+    public string tagName;
     public string sceneName;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag != "Player")
+        if(collision.gameObject.tag == tagName)
             SceneManager.LoadScene(sceneName);
     }
 }
