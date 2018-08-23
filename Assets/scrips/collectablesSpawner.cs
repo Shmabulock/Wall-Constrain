eb878afca8ee5ce4c099fd43a70dda3590067256
Shmabulock <dima.kahnovich@gmail.com>
2018-08-23 16:57:02 +0300

    bonuses(collectables)
    random  spawn of collectables
    some shit i dont remember

---
+++ Assets/scrips/collectablesSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectablesSpawner : MonoBehaviour
+{
+    [Range(1,10)]
+    public float delay;
+    public GameObject bonus;
+    public Camera mainCamera;
+    private int bonusCount;
+    private void Start()
+    {
+        bonusCount = 0;
+        StartCoroutine(Spawn(delay));
+    }
+    public IEnumerator Spawn(float delay) //SPAWN 
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            { 
+                Instantiate(bonus, new Vector2(Random.Range(mainCamera.transform.position.x - mainCamera.orthographicSize,
+                                                             mainCamera.transform.position.x + mainCamera.orthographicSize),
+                                               Random.Range(mainCamera.transform.position.y - mainCamera.orthographicSize,
+                                                             mainCamera.transform.position.y + mainCamera.orthographicSize)), Quaternion.identity);
+            }
+        }
+    }
+}
