0448812ba898e920164ada66f3ff451a934d9181
Shmabulock <dima.kahnovich@gmail.com>
2018-09-04 23:46:22 +0300

    freeze walls bonus
    vip

---
+++ Assets/scrips/wallFreezeBonusSpawner.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallFreezeBonusSpawner : MonoBehaviour
+{
+    [Range(1, 100)]
+    public float delay;
+    public GameObject bonus;
+    Camera mainCamera;
+    bool wallFreezeBonusCollected;
+
+    public GameObject frame;
+    GameObject leftWall;   
+    GameObject rightWall;   
+    GameObject upWall;
+    GameObject downWall;
+
+    float horizontalWallSpeed;
+    float verticalWallSpeed;
+    float freezeTime = 5f;
+
+
+
+    private void Start()
+    {
+        leftWall = GameObject.Find("frame/left");
+        rightWall = GameObject.Find("frame/right");      
+        upWall = GameObject.Find("frame/up");
+        downWall = GameObject.Find("frame/down");
+        mainCamera = Camera.main;
+        wallFreezeBonusCollected = false;
+
+        StartCoroutine(Spawn(delay));
+        StartCoroutine(unfreezeWalls());
+    }
+    public IEnumerator unfreezeWalls()
+    {
+        while(true)
+        {
+            if(wallFreezeBonusCollected)
+            {
+                yield return new WaitForSeconds(freezeTime);
+                leftWall.GetComponent<wallMove>().Speed = horizontalWallSpeed;
+                rightWall.GetComponent<wallMove>().Speed = horizontalWallSpeed;
+                upWall.GetComponent<wallMove>().Speed = verticalWallSpeed;
+                downWall.GetComponent<wallMove>().Speed = verticalWallSpeed;
+                wallFreezeBonusCollected = false;
+            }
+        }
+    }
+    public IEnumerator Spawn(float delay) //SPAWN 
+    {
+        while (true)
+        {
+            if (!wallFreezeBonusCollected)
+            {
+                delay += Random.Range(-20, 20);
+                yield return new WaitForSeconds(delay);
+                addBonus(bonus);
+            }
+        }
+
+    }
+    void addBonus(GameObject bonus)
+    {
+        Instantiate(bonus, new Vector2(Random.Range(mainCamera.transform.position.x - mainCamera.orthographicSize / 3,
+                                                     mainCamera.transform.position.x + mainCamera.orthographicSize / 3),
+                                       Random.Range(mainCamera.transform.position.y - mainCamera.orthographicSize / 3,
+                                                     mainCamera.transform.position.y + mainCamera.orthographicSize / 3)), Quaternion.identity);
+    }
+    public void setBonusCollected(bool a)
+    {
+        wallFreezeBonusCollected = a;
+    }
+    public void setHorizontalWallSpeed(float speed)
+    {
+        horizontalWallSpeed = speed;
+    }
+    public void setVerticalWallSpeed(float speed)
+    {
+        horizontalWallSpeed = speed;
+    }
+    public void setFreezeTime(float time)
+    {
+        freezeTime = time;
+    }
+}
---
+++ Assets/scrips/wallFreezeBonusTrigger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class wallFreezeBonusTrigger : MonoBehaviour
+{
+    public string wallsTagName;
+    public string playerTagName;
+    public GameObject frame;
+    public float freezeTime;
+    wallFreezeBonusSpawner wallFreezeSpawner;
+    GameObject leftWall;
+    GameObject rightWall;
+    GameObject upWall;
+    GameObject downWall;
+
+    Vector3 vec;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        leftWall = GameObject.Find("frame/left");
+        rightWall = GameObject.Find("frame/right");
+        upWall = GameObject.Find("frame/up");
+        downWall = GameObject.Find("frame/down");
+
+        wallFreezeSpawner = GameObject.FindObjectOfType<wallFreezeBonusSpawner>();
+
+        if (collision.gameObject.tag == wallsTagName)
+        {
+            Destroy(this.gameObject);
+        }
+        if (collision.gameObject.tag == playerTagName)
+        {
+            wallFreezeSpawner.GetComponent<wallFreezeBonusSpawner>().setHorizontalWallSpeed(leftWall.GetComponent<wallMove>().Speed);
+            wallFreezeSpawner.GetComponent<wallFreezeBonusSpawner>().setVerticalWallSpeed(upWall.GetComponent<wallMove>().Speed);
+            wallFreezeSpawner.GetComponent<wallFreezeBonusSpawner>().setFreezeTime(freezeTime + Random.Range(-1f,1f));
+            leftWall.GetComponent<wallMove>().Speed = 0;
+            rightWall.GetComponent<wallMove>().Speed = 0;
+            upWall.GetComponent<wallMove>().Speed = 0;
+            downWall.GetComponent<wallMove>().Speed = 0;
+
+            wallFreezeSpawner.setBonusCollected(true);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
+
