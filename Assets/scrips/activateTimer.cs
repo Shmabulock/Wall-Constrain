69fcd1cdfbf187148ca23db3851c3704bfb768e2
Shmabulock <dima.kahnovich@gmail.com>
2018-09-13 22:50:36 +0300

    time counting from the end of animation

---
+++ Assets/scrips/activateTimer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class activateTimer : MonoBehaviour {
+
+    public Text timerText;
+    public void setTimerActive()
+    {
+        timerText.gameObject.SetActive(true);
+        
+    }
+    public void setTimeScale(float a)
+    {
+        Time.timeScale = a;
+    }
+}
--- Assets/scrips/timer.cs
+++ Assets/scrips/timer.cs
@@ -8,29 +8,36 @@ public class timer : MonoBehaviour {
     private float myTimer;
 	// Use this for initialization
 	void Start () {
-        //Time.timeScale = 0;
+        Time.timeScale = 0;
+        TimerText.gameObject.SetActive(false);
         myTimer = 0;
         DontDestroyOnLoad(this);
-        TimerText = GameObject.Find("timerText").GetComponent<Text>();
+        if (TimerText.gameObject.activeSelf)
+            TimerText = GameObject.Find("timerText").GetComponent<Text>();
         
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Application.loadedLevelName == "gameplay")
+        if (TimerText.gameObject.activeSelf)
         {
-            myTimer += Time.deltaTime;
-          //  TimerText = GameObject.Find("timerText").GetComponent<Text>();
-           if(TimerText != null)
-            TimerText.text = myTimer.ToString("F");
-        }
-        if (Application.loadedLevelName == "gameOver")
-        {
-            TimerText = GameObject.Find("score").GetComponent<Text>();
-            TimerText.text = "Your time is " + myTimer.ToString("F") + " seconds";
+            if (Application.loadedLevelName == "gameplay")
+            {
+
+                myTimer += Time.deltaTime;
+                //  TimerText = GameObject.Find("timerText").GetComponent<Text>();
+                if (TimerText != null)
+                    TimerText.text = myTimer.ToString("F");
+            }
+            if (Application.loadedLevelName == "gameOver")
+            {
+                TimerText = GameObject.Find("score").GetComponent<Text>();
+                TimerText.text = "Your time is " + myTimer.ToString("F") + " seconds";
+            }
         }
         if(Application.loadedLevelName != "gameplay")
             Destroy(this.gameObject);
-
+        
     }
+
 }
