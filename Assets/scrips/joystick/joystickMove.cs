57016a774b1241d865215158fe0724922dc2c034
Shmabulock <dima.kahnovich@gmail.com>
2018-09-19 20:50:58 +0300

    player control, loadingScreen, found bug
    made second player control( commented in joustick control), loading screen for gameplay scene, bug in wall reset bonus spawner( randomly could be created a bunch of bonuses)

--- Assets/scrips/joystick/joystickMove.cs
+++ Assets/scrips/joystick/joystickMove.cs
@@ -15,34 +15,72 @@ public class joystickMove : MonoBehaviour
     private void FixedUpdate()
     {
 
-        if (Input.touchCount > 0)
+          if (Input.touchCount > 0)
+          {
+              Vector3 touch;
+
+               touch.x = Input.GetTouch(0).position.x + Input.GetTouch(0).deltaPosition.x;
+               touch.y = Input.GetTouch(0).position.y + Input.GetTouch(0).deltaPosition.y;
+               touch.z = 0f;
+
+              float MaxLength;
+              float deltaLength;
+              MaxLength = new Vector3(radius/2, radius, 0f).sqrMagnitude; //don't get it  why the half should be here but without it,
+                                                                          // joystick can be moved out of joystickBase bounds
+
+              Vector3 delta;
+              delta = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 0f) - joystickBase.position;
+              deltaLength = delta.sqrMagnitude;
+              delta.Normalize();
+              x = delta.x;
+              y = delta.y;
+              if (deltaLength >= MaxLength)
+              {
+               
+                  delta.Scale(new Vector3(radius, radius, 0));
+
+                  this.GetComponent<Transform>().position = joystickBase.position + delta;
+
+              }
+            else
+              {
+                  this.GetComponent<Transform>().position = touch;
+              }
+          }
+          else
+          {
+              this.GetComponent<Transform>().position = joystickBase.position;
+              x = 0;
+              y = 0;
+          }
+       /* if (Input.touchCount > 0)
         {
             Vector3 touch;
 
-             touch.x = Input.GetTouch(0).position.x + Input.GetTouch(0).deltaPosition.x;
-             touch.y = Input.GetTouch(0).position.y + Input.GetTouch(0).deltaPosition.y;
-             touch.z = 0f;
-            
+            touch.x = Input.GetTouch(0).position.x + Input.GetTouch(0).deltaPosition.x;
+            touch.y = Input.GetTouch(0).position.y + Input.GetTouch(0).deltaPosition.y;
+            touch.z = 0f;
+
             float MaxLength;
             float deltaLength;
-            MaxLength = new Vector3(radius/2, radius, 0f).sqrMagnitude; //don't get it  why the half should be here but without it,
-                                                                        // joystick can be moved out of joystickBase bounds
-       
+            MaxLength = new Vector3(radius / 2, radius, 0f).sqrMagnitude; //don't get it  why the half should be here but without it,
+                                                                          // joystick can be moved out of joystickBase bounds
+
             Vector3 delta;
             delta = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 0f) - joystickBase.position;
             deltaLength = delta.sqrMagnitude;
             delta.Normalize();
             x = delta.x;
-            y = delta.y;
+            y = delta.y;                    
             if (deltaLength >= MaxLength)
-            { 
-               
+            {
+                joystickBase.GetComponent<Transform>().position = (touch - delta);                      // used with no image like on all of the screen
                 delta.Scale(new Vector3(radius, radius, 0));
 
                 this.GetComponent<Transform>().position = joystickBase.position + delta;
-               
+
             }
-          else
+            else
             {
                 this.GetComponent<Transform>().position = touch;
             }
@@ -52,7 +90,30 @@ public class joystickMove : MonoBehaviour
             this.GetComponent<Transform>().position = joystickBase.position;
             x = 0;
             y = 0;
+        }*/
+        /*if (Input.touchCount > 0)
+        {
+            Vector3 touch;
+
+            touch.x = Input.GetTouch(0).deltaPosition.x;
+            touch.y = Input.GetTouch(0).deltaPosition.y;
+            touch.z = 0f;
+
+            float MaxLength;                    // ny takoe
+            float deltaLength;
+
+
+
+            touch.Normalize();
+            x = touch.x;
+            y = touch.y;
         }
+        else
+        {
+            x = 0;
+            y = 0;
+        }*/
+
     }
    public float getXAxis()
     {
---
+++ Assets/scrips/loadingScreenAsyncSceneLoad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadingScreenAsyncSceneLoad : MonoBehaviour {
+
+    public   string sceneName;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(loadScene());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator loadScene()
+    {
+        AsyncOperation loading = SceneManager.LoadSceneAsync(sceneName);
+
+        while(!loading.isDone)
+        {
+            yield return null;
+        }
+    }
+}
