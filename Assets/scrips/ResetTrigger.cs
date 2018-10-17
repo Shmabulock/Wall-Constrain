09a5acd44ad142f60c1f0e6d48096392a3a82072
Shmabulock <dima.kahnovich@gmail.com>
2018-10-17 19:05:11 +0300

    animation delay fiksed

---
+++ Assets/scrips/ResetTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	public void resetTrigger(string str)
+    {
+        this.GetComponent<Animator>().ResetTrigger(str);
+    }
+}
