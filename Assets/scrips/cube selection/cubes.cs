437acd03a3bd4c3c9c5a95f3ea530b90d94c178c
Shmabulock <dima.kahnovich@gmail.com>
2019-01-03 03:22:13 +0300

    buy locked cubes 0.5

--- Assets/scrips/cube selection/cubes.cs
+++ Assets/scrips/cube selection/cubes.cs
@@ -13,7 +13,7 @@ public class cubes : MonoBehaviour {
     public struct cube
     {
         public GameObject theCube;
-       public bool isLocked;
+        public bool isLocked;
     };
 
     cube[] allCubes;
@@ -22,27 +22,46 @@ public class cubes : MonoBehaviour {
     public Text CubeNumber;
 
     bool isSwiping = false;
-    float delta = 0;// maybe not wil be implemented so del
+    float delta = 0;
     Vector2 touchSwipeStartPos;
     public float swipelength;
+
+    public GameObject playButton;
+    public GameObject BuyButton;
 
 
     void Start () {
         numberOfCubes = cubesGameObj.transform.childCount;
         Debug.Log(numberOfCubes);
         allCubes = new cube[numberOfCubes];
+
+
         for(int i = 0; i < numberOfCubes; i++)
         {
-            allCubes[i].theCube = cubesGameObj.transform.GetChild(i).gameObject;
-            allCubes[i].isLocked = false;
+            if (!PlayerPrefs.HasKey("cubeID" + i.ToString() + "isLocked"))
+                PlayerPrefs.SetString("cubeID" + i.ToString() + "isLocked", "true");
+            if (PlayerPrefs.HasKey("cubeID" + i.ToString() + "isLocked"))
+            {
+                if(PlayerPrefs.GetString("cubeID" + i.ToString() + "isLocked") == "true")
+                {
+                    if (!PlayerPrefs.HasKey("cubeID" + i.ToString() + "price"))
+                        PlayerPrefs.SetInt("cubeID" + i.ToString() + "price", (i) * 500);
+                }
+            }
+                allCubes[i].theCube = cubesGameObj.transform.GetChild(i).gameObject;
+            allCubes[i].isLocked = PlayerPrefs.GetString("cubeID" + i.ToString() + "isLocked") == "true" ? true : false;
         }
+        PlayerPrefs.SetString("cubeID" + "0" + "isLocked", "false");
+        allCubes[0].isLocked = PlayerPrefs.GetString("cubeID" + "0" + "isLocked") == "true" ? true : false;
+
+
         isSwitching = false;
         if (PlayerPrefs.HasKey("selected"))
             selected = PlayerPrefs.GetInt("selected");
         else
             selected = 0;
         change();
-        this.GetComponent<Animator>().SetTrigger("decrease");
+        this.GetComponent<Animator>().SetTrigger("decrease"); // this is needed for the first cube rotation, without  it doesnt change cube in first button press
 
 
     }
@@ -67,12 +86,12 @@ public class cubes : MonoBehaviour {
                         delta = Mathf.Sign(delta);
                         if (delta < 0)
                         {
-                            loadLeft();
+                            loadRight();
 
                         }
                         else
                         {
-                            loadRight();
+                            loadLeft();
                         }
                     }
 
@@ -109,42 +128,42 @@ public class cubes : MonoBehaviour {
 
     public void loadLeft()
     {
-        if(selected > 0 && selected < numberOfCubes)
-        {
-            selected--;
-        }
-        else
-            if(selected == 0)
-            {
-                selected = numberOfCubes - 1;
-            }
-        Debug.Log("left ");
-        Debug.Log(selected);
-
         if (!isSwitching)
         {
             isSwitching = true;
+            if (selected > 0 && selected < numberOfCubes)
+            {
+                selected--;
+            }
+            else
+                if(selected == 0)
+                {
+                    selected = numberOfCubes - 1;
+                }
+            Debug.Log("left ");
+            Debug.Log(selected);
+
+       
             this.GetComponent<Animator>().SetTrigger("decrease");
         }
     }
-
     public void loadRight()
     {
-        
-        if(selected >=0 && selected < numberOfCubes-1)
-        {
-            selected++;
-        }
-        else
-            if(selected == numberOfCubes-1)
-            {
-                selected = 0;
-            }
-        Debug.Log("right ");
-        Debug.Log(selected);
         if (!isSwitching)
         {
             isSwitching = true;
+            if (selected >=0 && selected < numberOfCubes-1)
+            {
+                selected++;
+            }
+            else
+                if(selected == numberOfCubes-1)
+                {
+                    selected = 0;
+                }
+            Debug.Log("right ");
+            Debug.Log(selected);
+        
             this.GetComponent<Animator>().SetTrigger("decrease");
         }
     }
@@ -154,6 +173,7 @@ public class cubes : MonoBehaviour {
         cubeInfoKeeper.GetComponent<cubeKeeper>().sprite = this.GetComponent<Image>().sprite = allCubes[selected].theCube.GetComponent<SpriteRenderer>().sprite;
         cubeInfoKeeper.GetComponent<cubeKeeper>().material = this.GetComponent<Image>().material = allCubes[selected].theCube.GetComponent<SpriteRenderer>().sharedMaterial;
         cubeInfoKeeper.GetComponent<cubeKeeper>().color = this.GetComponent<Image>().color = allCubes[selected].theCube.GetComponent<SpriteRenderer>().color;
+        checkAvailabilty();
         this.GetComponent<Animator>().SetTrigger("increase");
         CubeNumber.text = selected+1 + "/" + numberOfCubes;
         
@@ -168,6 +188,30 @@ public class cubes : MonoBehaviour {
         this.GetComponent<Animator>().ResetTrigger("increase");
     }
    
+    public void checkAvailabilty()
+    {
+        if (allCubes[selected].isLocked)
+        {
+            Color locked = Color.gray;
+            locked.r = 0.3f;
+            locked.g = 0.3f;
+            locked.b = 0.3f;
+            locked.a = 1f;
+            this.GetComponent<Image>().color = locked;
+            BuyButton.SetActive(true);
+            playButton.SetActive(false);
+            BuyButton.GetComponent<Transform>().GetChild(1).GetComponent<Text>().text = "X" + PlayerPrefs.GetInt("cubeID" + selected + "price").ToString();
+        }
+        else
+        {
+            BuyButton.SetActive(false);
+            playButton.SetActive(true);
+        }
+    }
+    public void buySelectedCube()
+    {
+
+    }
 
    
 }
