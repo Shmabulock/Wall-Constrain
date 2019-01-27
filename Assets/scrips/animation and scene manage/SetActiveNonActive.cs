c0c71b1efb80d85699194e3a49f0884fe261bf2b
Shmabulock <dima.kahnovich@gmail.com>
2019-01-27 03:56:59 +0300

    tutorial done, yay

---
+++ Assets/scrips/animation and scene manage/SetActiveNonActive.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveNonActive : MonoBehaviour
+{
+
+    public void SetThisActive()
+    {
+
+        this.gameObject.SetActive(true);
+    }
+    public void SetThisNonActive()
+    {
+
+        //  this.gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/scrips/animation and scene manage/SetTriggerStr.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTriggerStr : MonoBehaviour {
+
+    public static void SetTrigger(Animator anim, string triggerName, bool state)
+    {
+        if(state)
+        {
+            anim.SetTrigger(triggerName);
+        }
+        else
+        {
+            anim.ResetTrigger(triggerName);
+        }
+    }
+}
